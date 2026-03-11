569c83361bae206719969876ac30a3a94dcba36f
Jaco Li <jellyfishjaco@JacodeMacBook-Pro.local>
2026-03-11 16:11:29 +0800

    fix(build): remove invalid eslint config and build cache dirs to unblock CI and bump v129

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    distDir: '.next_new',
     output: 'export',
-    eslint: {
-        ignoreDuringBuilds: true,
-    },
-    typescript: {
-        ignoreBuildErrors: true,
-    },
 };
 
 export default nextConfig;
